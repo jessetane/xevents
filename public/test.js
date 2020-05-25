c1083b8c949b2713d12a9c6ae49defd928f8d122
Jesse Tane <jesse.tane@gmail.com>
2020-05-24 20:50:49 -0400

    simplify testing

--- public/test.js
+++ public/test.js
@@ -1,15 +1,1 @@
-import {
-  Event,
-  CustomEvent,
-  EventTarget
-} from 'xevent/index.js'
-
-var emitter = new EventTarget()
-emitter.addEventListener('custom-event', e => {
-  console.log(e.type, e.detail, e.timeStamp, e instanceof Event, JSON.stringify(e, null, 2))
-  document.querySelector('body').innerHTML = `<pre>${e.type} ${e.detail} ${e.timeStamp} ${e instanceof Event} ${JSON.stringify(e, null, 2)}</pre>`
-})
-var evt = new CustomEvent('custom-event', { detail: 42 })
-setTimeout(() => {
-  emitter.dispatchEvent(evt)
-}, 250)
+../test.js
